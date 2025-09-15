8f5d9bff13c7979f837b4eb16aa61405484f542f
simonenieddu <51482458+simonenieddu@users.noreply.github.com>
2025-09-15 18:12:06 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
